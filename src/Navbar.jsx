f240a39c1990ec0ef38db5bc38bcc3e64c9fe68f
NaraeJang <j.naaaarae@gmail.com>
2023-09-28 15:28:32 -0400

    fixed setup

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import logo from "./logo.svg";
 const Navbar = () => {
-  const [showLinks, setShowLinks] = useState(true);
+  const [showLinks, setShowLinks] = useState(false);
 
   const toggleLinks = () => {
     setShowLinks(!showLinks);
@@ -17,20 +17,21 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container">
-            <ul className="links">
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+        <div
+          className={
+            showLinks ? "links-container show-container" : "links-container"
+          }>
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );
